ba92c0c4cce08ee0b3f007a5287bba8ad76a18ce
hhluci <hhluci@163.com>
2019-10-19 10:40:18 +0800

    u

--- src/main/java/com/bee/sample/client/Client.java
+++ src/main/java/com/bee/sample/client/Client.java
@@ -2,7 +2,12 @@ package com.bee.sample.client;
 
 import com.bee.sample.service.UserClientService;
 import com.bee.sample.service.impl.UserClientServiceImpl;
-
+/**
+ * 问题：分析类间的依赖关系，引出问题；
+ * 方案：为了减少它们之间的依赖需要采取解耦策略
+ * @author hhluci
+ *
+ */
 public class Client {
 
 	public static void main(String[] args) {
